a47f4934b918a4ce55f278855a94b34d2fca1b2a
PaweLewoniewski <pawel.lewoniewski@gmail.com>
2023-03-11 20:13:19 +0100

    rem_com

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -4,7 +4,6 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import BgCategory from "../../assets/img/categorymenu.webp";
 
 interface MenuCardProps {
   title:string;
